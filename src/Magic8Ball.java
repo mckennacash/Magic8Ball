7e04b23b1b04959808413f72800f1638055743b4
McKenna Cash <mckenna.cash.2020@student.lhschools.org>
2018-09-17 08:02:37 -0400

    Initialised Magic8Ball Variables

---
+++ src/Magic8Ball.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Magic8Ball {
+    public static void main(String[]args){
+
+    Scanner keyboard;
+    int testing;
+    String question;
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
