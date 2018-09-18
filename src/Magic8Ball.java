b5e4f4367be4c760f838d1e2bd218176c71541fc
McKenna Cash <mckenna.cash.2020@student.lhschools.org>
2018-09-18 08:13:47 -0400

    initialized coding

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -1,3 +1,5 @@
+import jdk.nashorn.internal.ir.IfNode;
+
 import java.util.Scanner;
 
 public class Magic8Ball {
@@ -7,13 +9,24 @@ public class Magic8Ball {
     int testing;
     String question;
 
+    keyboard = new Scanner(System.in);
 
+        System.out.println("Ask the Magic 8 Ball Question?");
+        keyboard.nextLine();
+        System.out.println("Ask your question below");
+       question = keyboard.nextLine();
+       System.out.println("Now please input a number from 1 to 100");
+       testing = keyboard.nextInt();
+        if (testing<=10) {
+            System.out.println("That's the tea sis");
+        }
 
-
-
-
-
-
+        else if (testing<=20){
+            System.out.println("Sources point to no sishster");
+        }
+        else if (testing<=30){
+            System.out.println("Snatch her wig");
+        }
 
 
 
